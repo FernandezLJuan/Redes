b498f2fe2f49ab350ea5bc13d7bc1a908c8eb13e
FernandezLJuan <juanlopezfernandes@gmail.com>
2022-11-08 20:44:32 +0100

    Codigos de servidor 11 y cliente 11 funcionales, no robustos ni comentados

--- practica2/cliente-11.py
+++ practica2/cliente-11.py
@@ -1,6 +1,26 @@
 import socket
 import sys,os
+from time import sleep
 
+IP=0
+porto=0
+
+for i in sys.argv[1:]:
+	try:
+	
+		porto=int(i)
+		
+	except ValueError:
+	
+		IP=i
+		
 socketCliente=socket.socket(socket.AF_INET,socket.SOCK_STREAM,fileno=None)
-socketCliente.connect(('',37001))
+socketCliente.connect(('',porto))
 
+sleep(1)
+print(socketCliente.recv(1024).decode())
+
+print("Rematado")
+mensaje='AAAAAAAA AAAAAA'
+socketCliente.send(mensaje.encode())
+socketCliente.close()
--- practica2/servidor-11.py
+++ practica2/servidor-11.py
@@ -1,16 +1,31 @@
 import socket
 import sys,os
 
+porto=sys.argv[1:]
+
 socketServer=socket.socket(socket.AF_INET,socket.SOCK_STREAM, 0, fileno=None)
 
 cadea='Hola mundo'
 
-socketServer.bind(('',37001))
+socketServer.bind(('0.0.0.0',int(porto[0])))
 
 socketServer.listen(4)
 
-while 1:
+while True:
 
 	socketConexion,direcion=socketServer.accept()
 	print("Conexion aceptada do cliente con direción: ",direcion[0])
-	socketServer.close()
+	mensaxe1='Hola, este es el servidor,'
+	mensaxe2=' la conexion ha sido establecida correctamente'
+
+	socketConexion.send(mensaxe1.encode())
+	socketConexion.send(mensaxe2.encode())
+	
+	mensaxe_recibida=socketConexion.recv(8)
+
+	print("Mensaxe recibida do servidor {}".format(mensaxe_recibida.decode()))
+
+
+print('Cerramos la conexión')
+socketConexion.close()
+socketServer.close()
